ab8a1e6ca40b71eb59259f7f7a5bec41f8c87dc5
ArthurTrauter <arthur.trauter@googlemail.com>
2014-10-16 07:57:34 +0200

    deleted old files of old structure

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@
   module.exports = function (app, express) {
 
       var router  = express.Router();
-      
+
       // middleware to use for all requests
       router.use(function (req, res, next) {
           console.log('Something is happening');
