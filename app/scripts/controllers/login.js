5a6451351b460cc6318c99b3855e6629f7823bc1
ArthurTrauter <arthur.trauter@googlemail.com>
2014-10-16 07:58:11 +0200

    commited old files the second commit ;)

--- app/scripts/controllers/login.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-bmApp.controller('LoginCtrl', function ($scope) {
-    $scope.isLoginView = true;
-});
--- app/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-});
